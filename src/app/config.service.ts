cd78514208fca11760edae959ea17cdcf4e8b090
Matias Culasso Paulin <matiasculasso@gmail.com>
2018-01-11 11:03:45 -0300

    Adding newest files

---
+++ src/app/config.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { OpenIDImplicitFlowConfiguration } from 'angular-auth-oidc-client';
+
+@Injectable()
+export class ConfigService {
+
+    GetOpenIdConfig (): OpenIDImplicitFlowConfiguration {
+        const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+        openIDImplicitFlowConfiguration.stsServer = 'http://localhost:12000';
+        openIDImplicitFlowConfiguration.redirect_url = 'http://localhost:4200/';
+        openIDImplicitFlowConfiguration.client_id = 'clinic-hitstory';
+        openIDImplicitFlowConfiguration.response_type = 'id_token token';
+        openIDImplicitFlowConfiguration.scope = 'openid profile';
+        openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'http://localhost:4200/';
+        openIDImplicitFlowConfiguration.start_checksession = false;
+        openIDImplicitFlowConfiguration.silent_renew = true;
+        openIDImplicitFlowConfiguration.silent_renew_offset_in_seconds = 0;
+        openIDImplicitFlowConfiguration.post_login_route = '/home';
+        openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+        openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+        openIDImplicitFlowConfiguration.auto_userinfo = true;
+        openIDImplicitFlowConfiguration.log_console_warning_active = true;
+        openIDImplicitFlowConfiguration.log_console_debug_active = false;
+        openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 10;
+        openIDImplicitFlowConfiguration.override_well_known_configuration = false;
+        openIDImplicitFlowConfiguration.override_well_known_configuration_url = 'http://localhost:12000/.well-known/openid-configuration/';
+        openIDImplicitFlowConfiguration.storage = localStorage;
+
+        // if you need custom parameters
+        // oidcSecurityService.setCustomRequestParameters({ 't4': 'ABC abc 123', 't3': 'wo' });
+
+        return openIDImplicitFlowConfiguration;
+    }
+}

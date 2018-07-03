6689da93e2f56d6fccba1540bf4e466edcade7d9
Matias Culasso Paulin <matiasculasso@gmail.com>
2018-07-02 23:59:45 -0300

    adding https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   API_ENDPOINT: 'https://clinic-history-api.azurewebsites.net/api/',
-  CLIENT_URL: 'http://clinic-history-app.herokuapp.com',
+  CLIENT_URL: 'https://clinic-history-app.herokuapp.com',
   SERVER_URL: 'https://clinic-history-api.azurewebsites.net'
 };
